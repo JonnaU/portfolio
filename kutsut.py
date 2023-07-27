96b26757fb57d54e32d1ff024da1af1439251812
Jonna Humaloja <93709971+JonnaU@users.noreply.github.com>
2023-07-27 08:56:59 +0300

    Add files via upload

--- kutsut.py
+++ kutsut.py
@@ -1,6 +1,8 @@
 import requests
 from pprint import pprint
-def työpaikat():
+
+def työpaikat_tpt():
+    """ Haetaan työpaikat työvoimatoimiston APIsta"""
     headers = {
         'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Microsoft Edge";v="114"',
         'Accept': 'application/json, text/plain, */*',
@@ -16,3 +18,19 @@ def työpaikat():
     )
     pprint(response.json())
     return response.json()
+
+def työpaikat_duunitori(haku):
+    """ Haetaan työpaikat Duunitorin APIsta"""
+    headers = {
+        'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Microsoft Edge";v="114"',
+        'Accept': 'application/json, text/plain, */*',
+        'Referer': 'https://paikat.te-palvelut.fi/tpt/',
+        'sec-ch-ua-mobile': '?0',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1823.82',
+        'sec-ch-ua-platform': '"Windows"',
+    }
+    response = requests.get(
+        f'https://duunitori.fi/carousel/search?alue=Pirkanmaa&amp;haku={haku}',
+        headers=headers,
+    )
+    return response.text
